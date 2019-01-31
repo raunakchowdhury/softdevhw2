6c91a38e15cc70443feb823c34bac6cc761100f1
raunakchowdhury <rchowdhury5@stuy.edu>
2019-01-31 14:28:38 -0500

    started 01

---
+++ 01_canvas/clickdraw.js
@@ -0,0 +1,56 @@
+// Raunak Chowdhury
+// SoftDev2 pd8
+// K00 -- I See a Red Door...
+// 2019-01-30
+
+var canvas = document.getElementById("slate");
+var clear = document.getElementById("clear");
+var toggle = document.getElementById("toggle");
+
+// instantiate a canvas
+var ctx = canvas.getContext("2d");
+
+// state var that determines if box (0) or if circle (1)
+var state = 0;
+
+var clear_canvas = function(e){
+  ctx.clearRect(0,0,600,600);
+}
+
+var add_shape = function(e){
+  e.preventDefault();
+  if (state == 0){
+    ctx.fillStyle = "#00FF00";
+    ctx.fillRect(e.offsetX,e.offsetY,50,50);
+  }
+  else{
+    console.log("filling ellipse");
+    ctx.fillStyle = "#FF0000";
+    // need this to prevent fill() from causing wonky shapes to appear on the screen
+    ctx.beginPath();
+    ctx.ellipse(e.offsetX,e.offsetY,50, 50, 0, 0, 2 * Math.PI);
+    // ellipse is invisible until you fill it!
+    ctx.fill();
+  }
+}
+
+var change_toggle = function(e){
+  if (state == 0){
+    state = 1;
+  }
+  else{
+    state = 0;
+  }
+}
+
+clear.addEventListener('click', clear_canvas);
+toggle.addEventListener('click', change_toggle);
+canvas.addEventListener('click', add_shape);
+
+/*
+ * Explanations:
+ * ctx.beginPath(): resets a "pen" used to draw shapes
+ * e.preventDefault(): after further testing, no changes were observed. I'm unsure what it does as of yet.
+ * e.offsetX: x-coordinate of mouse pointer when an event is triggered
+ * e.offsetY: y-coordinate of mouse pointer when an event is triggered
+ */
